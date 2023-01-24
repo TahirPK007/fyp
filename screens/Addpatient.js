95d2ae540f881840c6e76c6506ccc17a7cce6ad5
Tahir Mehmood <tahir.mehmood9425@gmail.com>
2023-01-24 22:57:42 +0500

    added some code to add patient comp

--- screens/Addpatient.js
+++ screens/Addpatient.js
@@ -1,12 +1,45 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, } from 'react-native'
+import Reac, { useState, useEffect } from 'react'
+import { TextInput } from 'react-native-paper';
+
 
 const Addpatient = () => {
+
+  const [cnic, setcnic] = useState()
+  const [relation, setrelation] = useState()
+
   return (
-    <View>
-      <Text>Addpatient</Text>
+    <View style={{ felx: 1 }}>
+      <View style={{ width: "100%", justifyContent: "center", alignItems: "center" }}>
+        <Text style={styles.txt}>Add New Patient</Text>
+      </View>
+      <Text style={{ color: "red", fontSize: 20 }}>Cnic</Text>
+      <TextInput
+        // label="Cnic"
+        value={cnic}
+        onChangeText={text => setcnic(text)}
+      />
+      <View>
+        <Text style={{ color: "red", fontSize: 20 }}>Full Name</Text>
+        <TextInput
+          // label="Cnic"
+          value={cnic}
+          onChangeText={text => setcnic(text)}
+        />
+      </View>
+      <View>
+
+      </View>
     </View>
   )
 }
 
 export default Addpatient;
+
+const styles = StyleSheet.create({
+  txt: {
+    fontWeight: "bold",
+    fontSize: 25,
+    marginTop: 20,
+  }
+})
